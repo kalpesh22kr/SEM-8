799331badca765f34b05ee9c4aa07d6c5b6d8581
Kalpesh Baviskar <baviskarkalpuo3@gmail.com>
2025-05-07 23:24:15 +0530

    Update bfs.cpp

--- HPC/bfs.cpp
+++ HPC/bfs.cpp
@@ -81,6 +81,45 @@ private:
         }
     }
 
+    void bfs_sequential(int startVertex) {
+        vector<bool> visited(numVertices, false);
+        queue<int> q;
+
+        visited[startVertex] = true;
+        q.push(startVertex);
+
+        cout << "\nParallel BFS Output:\n";
+
+        while (!q.empty()) {
+            int currentVertex = q.front();
+            q.pop();
+    
+            
+                cout << currentVertex << endl;
+    
+            // Gather neighbors to process in parallel safely
+            vector<int> neighbors = adj[currentVertex];
+            for (int i = 0; i < neighbors.size(); i++) {
+                int neighbor = neighbors[i];
+                int thread_id = omp_get_thread_num();
+    
+                bool shouldVisit = false;
+    
+                // Synchronize check-and-mark
+                    if (!visited[neighbor]) {
+                        visited[neighbor] = true;
+                        q.push(neighbor);
+                        shouldVisit = true;
+                    }
+    
+            
+                    
+                    cout << "Thread " << thread_id << " visiting node " << neighbor << endl;
+            }
+
+        }
+    }
+
     // Helper for parallel DFS
     void dfs(int startVertex) {
         vector<bool> visited(numVertices, false);
@@ -129,6 +168,48 @@ private:
             }
         }
     }
+
+    void dfs_sequential(int startVertex) {
+        vector<bool> visited(numVertices, false);
+        stack<int> s;
+    
+        visited[startVertex] = true;
+        s.push(startVertex);
+    
+        cout << "\nDFS Output:\n";
+    
+        while (!s.empty()) {
+            int currentVertex = s.top();
+            s.pop();
+    
+
+                cout << currentVertex << endl;
+    
+            // Gather neighbors to process in parallel safely
+            vector<int> neighbors = adj[currentVertex];
+    
+           
+            for (int i = 0; i < neighbors.size(); i++) {
+                int neighbor = neighbors[i];
+                int thread_id = omp_get_thread_num();
+    
+                bool shouldVisit = false;
+    
+                // Synchronize check-and-mark
+                
+                    if (!visited[neighbor]) {
+                        visited[neighbor] = true;
+                        s.push(neighbor);
+                        shouldVisit = true;
+                    }
+    
+            
+                    
+                cout << "Thread " << thread_id << " visiting node " << neighbor << endl;
+                
+            }
+        }
+    }
      
 };
 
@@ -154,18 +235,29 @@ int main() {
     int startVertex;
     cout << "Enter the starting vertex: ";
     cin >> startVertex;
+   
     double startTime = omp_get_wtime();
-
-    graph.bfs(startVertex);
-    graph.dfs(startVertex);
-
+    graph.bfs_sequential(startVertex);
     double endTime = omp_get_wtime();
+    cout << "\nTotal time for BFS Sequential: " << (endTime - startTime) << " seconds" << endl;
 
-    cout << "\nTotal time for BFS + DFS: " << (endTime - startTime) << " seconds" << endl;
+    double startTime = omp_get_wtime();
+    graph.bfs(startVertex);
+    double endTime = omp_get_wtime();
+    cout << "\nTotal time for BFS Parallel: " << (endTime - startTime) << " seconds" << endl;
+
+    double startTime = omp_get_wtime();
+    graph.dfs_sequential(startVertex);
+    double endTime = omp_get_wtime();
+    cout << "\nTotal time for DFS Sequential: " << (endTime - startTime) << " seconds" << endl;
+
+    double startTime = omp_get_wtime();
+    graph.dfs(startVertex);
+    double endTime = omp_get_wtime();
+    cout << "\nTotal time for DFS Parallel: " << (endTime - startTime) << " seconds" << endl;
 
     return 0;
 }
-
 
 // Enter number of vertices: 7
 // Enter number of edges: 6
